5158112f627dbe97d3707e09465f431f9c08387e
Kacper Papuga <kacperpap@gmail.com>
2024-08-22 10:32:19 +0200

    Error page

--- frontend/src/features/Routing.tsx
+++ frontend/src/features/Routing.tsx
@@ -2,6 +2,8 @@ import { Navigate, RouteObject, useRoutes } from "react-router-dom"
 import { Layout } from "../components/Layout"
 import Map from "./map/Map"
 import { Login } from "./login/Login"
+import { DroneInput } from "./drone/DroneInput"
+import { ErrorPage } from "./error/ErrorPage"
 
 
 const publicRoutes: RouteObject[] = [
@@ -12,12 +14,20 @@ const publicRoutes: RouteObject[] = [
             {
                 path: '/',
                 element: <Map />
+            },
+            {
+                path: '/drone-input',
+                element: <DroneInput />
             }
         ]
     },
     {
         path: '/login',
         element: <Login />
+    },
+    {
+        path: '*',
+        element: <ErrorPage />
     }
 ]
 
