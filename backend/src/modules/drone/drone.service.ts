d02726e0356616274cdd4f7b795c2c08e6c3f7ee
Kacper <kacperpap@gmail.com>
2024-08-28 23:18:57 +0200

    Drone service updateDroneFlight change

--- backend/src/modules/drone/drone.service.ts
+++ backend/src/modules/drone/drone.service.ts
@@ -1,16 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateDroneFlightType } from './dto/create-drone-flight';
 import { EditDroneFlightType } from './dto/edit-drone-flight';
 import { PrismaService } from '../prisma/prisma.service';
+import { DroneFlightType } from './dto/drone-flight';
 
 @Injectable()
 export class DroneService {
     constructor(private readonly prisma: PrismaService) {}
 
-    async getAllDroneFlights(userId: number) {
+    /*
+    // There is a way to return defined type not json as below, but this may cause problems on frontend
+
+    async getAllDroneFlights(userId: number): Promise<DroneFlightType[]> {
+        const droneFlights = await this.prisma.droneFlight.findMany({
+            where: { userId },
+            include: {
+                measurements: true, 
+            },
+        });
+
+        return plainToInstance(DroneFlightType, droneFlights, {
+            excludeExtraneousValues: true, // Automatyczne wykluczanie `@Exclude`
+        });
+    }
+    */
+
+    async getAllDroneFlights(userId: number){
         return this.prisma.droneFlight.findMany({
             where: {
                 userId: userId
+            }, 
+            include: {
+                measurements: true
             }
         })
     }
@@ -20,6 +41,9 @@ export class DroneService {
             where: {
                 id: id,
                 userId: userId
+            }, 
+            include: {
+                measurements: true
             }
         })
     }
@@ -27,12 +51,22 @@ export class DroneService {
     async addDroneFlight(data: CreateDroneFlightType, userId: number) {
         return this.prisma.droneFlight.create({
             data: {
-                ...data,
-                userId: userId
+                title: data.title,
+                description: data.description,
+                date: data.date,
+                userId: userId,
+                measurements: {
+                    create: data.measurements.map(measurement => ({
+                        name: measurement.name,
+                        latitude: measurement.latitude,
+                        longitude: measurement.longitude,
+                        temperature: measurement.temperature,
+                    }))
+                }
             }
-        })
+        });
     }
-
+    
 
     async deleteDroneFlight(userId: number, id: number) {
         return this.prisma.droneFlight.delete({
@@ -45,15 +79,37 @@ export class DroneService {
 
 
     async editDroneFlight(data: EditDroneFlightType, userId: number, id: number) {
+    /*
+    Edit drone flight takes new data: EditDroneFlightType and updates all variables,
+    also updates measurements: EditDroneFlightMeasurementType, which means, it removes all
+    and add new defined in formula
+    */
+        
+        await this.prisma.droneMeasurement.deleteMany({
+            where: {
+                flightId: id
+            }
+        });
+
         return this.prisma.droneFlight.update({
             where: {
                 userId: userId,
                 id: id
-            }, 
+            },
             data: {
-                ...data
+                title: data.title,
+                description: data.description,
+                date: data.date,
+                measurements: {
+                    create: data.measurements.map(measurement => ({
+                        name: measurement.name,
+                        latitude: measurement.latitude,
+                        longitude: measurement.longitude, 
+                        temperature: measurement.temperature,
+                    }))
+                }
             }
-        })
+        });
     }
     
     
