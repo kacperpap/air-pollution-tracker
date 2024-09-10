1fa30e148dfc9835b3e712b5c1cf9115ae0e75a1
Kacper Papuga <kacperpap@gmail.com>
2024-09-10 16:13:49 +0200

    Working on concentration simulation

--- calc_model/models/euler_volume_muliboxes_model/concentration_simulation.py
+++ calc_model/models/euler_volume_muliboxes_model/concentration_simulation.py
@@ -1,6 +1,8 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
-from grid import create_multibox_grid_with_interpolated_measurements
+from datetime import datetime
+from grid_and_interpolation import create_multibox_grid_with_interpolated_measurements
 
 data_2 = [
     {
@@ -8,7 +10,8 @@ data_2 = [
         "name": "Point 1",
         "latitude": 23.12,
         "longitude": 23.1,
-        "some_value_1": 1,
+        "wind_speed": 2.1,   
+        "wind_direction": 45,
         "temperature": 32.21
     },
     {
@@ -16,7 +19,8 @@ data_2 = [
         "name": "Point 2",
         "latitude": 23.13,
         "longitude": 24,
-        "some_value_1": 1,
+        "wind_speed": 2.2,   
+        "wind_direction": 56,
         "temperature": 32.2
     },
     {
@@ -24,7 +28,8 @@ data_2 = [
         "name": "Point 3",
         "latitude": 23.14,
         "longitude": 23.8,
-        "some_value_1": 1,
+        "wind_speed": 1.8,   
+        "wind_direction": 33,
         "temperature": 32.19
     },
     {
@@ -32,7 +37,8 @@ data_2 = [
         "name": "Point 4",
         "latitude": 23.15,
         "longitude": 23.5,
-        "some_value_1": 1,
+        "wind_speed": 2.3,   
+        "wind_direction": 48,
         "temperature": 32.18
     },
     {
@@ -40,7 +46,8 @@ data_2 = [
         "name": "Point 5",
         "latitude": 23.15,
         "longitude": 23.8,
-        "some_value_1": 1,
+        "wind_speed": 2.0,   
+        "wind_direction": 102,
         "temperature": 32.21
     },
     {
@@ -48,7 +55,8 @@ data_2 = [
         "name": "Point 6",
         "latitude": 23.18,
         "longitude": 23.6,
-        "some_value_1": 1,
+        "wind_speed": 2.2,   
+        "wind_direction": 79,
         "temperature": 32.3
     }
 ]
@@ -61,6 +69,8 @@ data = [
     "latitude": 50.0545,
     "longitude": 19.9353,
     "temperature": 22.5,
+    "wind_speed": 2.5,   
+    "wind_direction": 45,
     "flightId": 6
   },
   {
@@ -69,6 +79,8 @@ data = [
     "latitude": 50.0614,
     "longitude": 19.9372,
     "temperature": 23.1,
+    "wind_speed": 3.0,   
+    "wind_direction": 90,
     "flightId": 6
   },
   {
@@ -77,6 +89,8 @@ data = [
     "latitude": 50.0487,
     "longitude": 19.9445,
     "temperature": 22.8,
+    "wind_speed": 2.0,   
+    "wind_direction": 135,
     "flightId": 6
   },
   {
@@ -85,6 +99,8 @@ data = [
     "latitude": 50.051,
     "longitude": 19.9366,
     "temperature": 22.3,
+    "wind_speed": 1.8,   
+    "wind_direction": 60,
     "flightId": 6
   },
   {
@@ -93,6 +109,8 @@ data = [
     "latitude": 50.0647,
     "longitude": 19.9248,
     "temperature": 23.4,
+    "wind_speed": 2.6,   
+    "wind_direction": 120,
     "flightId": 6
   },
   {
@@ -101,6 +119,8 @@ data = [
     "latitude": 50.0616,
     "longitude": 19.9393,
     "temperature": 23.0,
+    "wind_speed": 2.8,   
+    "wind_direction": 100,
     "flightId": 6
   },
   {
@@ -109,6 +129,8 @@ data = [
     "latitude": 50.0594,
     "longitude": 19.9336,
     "temperature": 22.7,
+    "wind_speed": 1.8,   
+    "wind_direction": 78,
     "flightId": 6
   },
   {
@@ -117,6 +139,8 @@ data = [
     "latitude": 50.0642,
     "longitude": 19.942,
     "temperature": 22.9,
+    "wind_speed": 2.1,   
+    "wind_direction": 108,
     "flightId": 6
   },
   {
@@ -125,6 +149,8 @@ data = [
     "latitude": 50.0636,
     "longitude": 19.9383,
     "temperature": 23.2,
+    "wind_speed": 2.8,   
+    "wind_direction": 132,
     "flightId": 6
   },
   {
@@ -133,6 +159,8 @@ data = [
     "latitude": 50.0617,
     "longitude": 19.9115,
     "temperature": 23.3,
+    "wind_speed": 2.3,   
+    "wind_direction": 34,
     "flightId": 6
   },
   {
@@ -141,6 +169,8 @@ data = [
     "latitude": 50.07,
     "longitude": 20.0374,
     "temperature": 23.0,
+    "wind_speed": 2.2,   
+    "wind_direction": 22,
     "flightId": 6
   },
   {
@@ -149,6 +179,8 @@ data = [
     "latitude": 49.9873,
     "longitude": 20.0652,
     "temperature": 22.6,
+    "wind_speed": 2.2,   
+    "wind_direction": 78,
     "flightId": 6
   }
 ]
@@ -193,8 +225,52 @@ def update_concentration(C, u, v, K_x, K_y, dx, dy, dt, S_c, nx, ny):
     C_new = C + dC_dt * dt
     return C_new
 
+def plot_concentration_grid(boxes, concentration_values, measurements, save_image=False, image_path=None):
+    """
+    Rysuje siatkę pudełek z interpolowanymi/obliczonymi w kolejnych krokach symulacji wartościami stężeń oraz opcjonalnie zapisuje obraz.
+    
+    Parametry:
+    - boxes: lista granic geograficznych pudełek [(lat_min, lat_max, lon_min, lon_max), ...]
+    - concentration_values: wartości stężeń przypisane do pudełek (None dla pustych pudełek)
+    - measurements: lista punktów pomiarowych
+    - save_image: boolean, jeśli True, zapisuje obraz do pliku
+    - image_path: ścieżka do pliku, w którym zapisany zostanie obraz (jeśli save_image=True)
+    """
+    
+    fig, ax = plt.subplots(figsize=(10, 8))
 
-def simulate_pollution_spread(data, num_steps, dt, min_box_size=0.001, max_box_size=0.02, margin=0.005):
+    valid_values = [value for value in concentration_values if value is not None]
+    if valid_values:
+        min_value, max_value = min(valid_values), max(valid_values)
+    else:
+        min_value, max_value = 0, 1
+
+    for i, (lat_min, lat_max, lon_min, lon_max) in enumerate(boxes):
+        color_value = concentration_values[i]
+        if color_value is not None:
+            normalized_value = (color_value - min_value) / (max_value - min_value)
+            color = plt.cm.plasma(normalized_value)  
+        else:
+            color = 'lightgrey'
+        
+        rect = plt.Rectangle((lon_min, lat_min), lon_max - lon_min, lat_max - lat_min, 
+                             linewidth=1, edgecolor='blue', facecolor=color)
+        ax.add_patch(rect)
+
+    latitudes = np.array([point["latitude"] for point in measurements])
+    longitudes = np.array([point["longitude"] for point in measurements])
+    plt.scatter(longitudes, latitudes, c='black', edgecolor='k', s=100, zorder=5)
+
+    ax.set_xlabel('Longitude')
+    ax.set_ylabel('Latitude')
+    plt.colorbar(label='Pollutant Concentration')
+    plt.grid(True)
+
+    if save_image and image_path:
+        plt.savefig(image_path)
+
+
+def simulate_pollution_spread(data, num_steps, dt, min_box_size=0.001, max_box_size=0.02, margin=0.005, debug=False):
     """
     Symuluje rozprzestrzenianie się zanieczyszczeń w powietrzu w modelu eulerowskim.
 
@@ -208,7 +284,19 @@ def simulate_pollution_spread(data, num_steps, dt, min_box_size=0.001, max_box_s
     Zwraca:
     - Ostateczne stężenie zanieczyszczeń po symulacji
     """
-    boxes, initial_concentration = create_multibox_grid_with_interpolated_measurements(data, min_box_size=min_box_size, max_box_size=max_box_size, margin=margin)
+
+    if debug is False:
+      boxes, temp_values, u_values, v_values = create_multibox_grid_with_interpolated_measurements(
+        data, min_box_size=min_box_size, max_box_size=max_box_size, margin=margin)
+    else:
+      timestamp = datetime.now().strftime("%Y_%m_%d_%H%M%S")
+      directory = f'models/euler_volume_muliboxes_model/debug/{timestamp}/plots/'
+
+      if not os.path.exists(directory):
+        os.makedirs(directory)
+
+      boxes, temp_values, u_values, v_values = create_multibox_grid_with_interpolated_measurements(
+        data, min_box_size=min_box_size, max_box_size=max_box_size, margin=margin, save_grid_images=True, save_path=directory) 
 
     x_coords = sorted(set([box[0] for box in boxes]))
     y_coords = sorted(set([box[2] for box in boxes]))
@@ -216,43 +304,38 @@ def simulate_pollution_spread(data, num_steps, dt, min_box_size=0.001, max_box_s
     nx = len(x_coords)
     ny = len(y_coords)
 
-    # Początkowe wartości stężeń, prędkości wiatru i dyfuzji
-    C = np.array(initial_concentration, dtype=float).reshape((nx, ny))    
-    u = np.random.uniform(1, 3, size=(nx, ny))  # losowa prędkość wiatru w kierunku x
-    v = np.random.uniform(1, 3, size=(nx, ny))  # losowa prędkość wiatru w kierunku y
-    K_x = 0.1  # współczynnik dyfuzji w kierunku x
-    K_y = 0.1  # współczynnik dyfuzji w kierunku y
-    S_c = np.zeros((nx, ny))  # brak dodatkowych źródeł emisji na początku
+    C = np.random.uniform(0, 1, size=(nx, ny)) 
 
-    # Rozmiary pudełek (zakładamy, że wymiary są stałe w obrębie siatki)
-    dx = np.mean([box[1] - box[0] for box in boxes])  # średnia szerokość pudełka w kierunku x
-    dy = np.mean([box[3] - box[2] for box in boxes])  # średnia szerokość pudełka w kierunku y
+    u = np.array(u_values).reshape((nx, ny))  
+    v = np.array(v_values).reshape((nx, ny)) 
 
+    K_x = 0.1  
+    K_y = 0.1 
 
-    # Symulacja w pętli czasowej
+    dx = (x_coords[1] - x_coords[0])  
+    dy = (y_coords[1] - y_coords[0]) 
+
+    S_c = np.zeros_like(C)
+    S_c[0, 0] = 1  
+
+    if debug is True:
+      image_path = f'models/euler_volume_muliboxes_model/debug/{timestamp}/plots/start_pollutant_concentration_grid.png'
+      plot_concentration_grid(boxes, C.flatten(), data, True, image_path)
+
     for step in range(num_steps):
         C = update_concentration(C, u, v, K_x, K_y, dx, dy, dt, S_c, nx, ny)
+        if debug is True:
+           # create and save debug files from each step
+           pass
         
-        # Opcjonalnie: wizualizacja co kilka kroków
-        if step % 10 == 0:
-            plt.imshow(C, cmap='hot', interpolation='nearest')
-            plt.colorbar()
-            plt.title(f"Stężenie zanieczyszczeń - krok {step}")
-            plt.show()
-
+    if debug is True and step == num_steps-1:
+      image_path = f'models/euler_volume_muliboxes_model/debug/{timestamp}/plots/final_pollutant_concentration_grid.png'
+      plot_concentration_grid(boxes, C.flatten(), data, True, image_path)
+       
     return C
 
 
+num_steps = 100  
+dt = 0.01  
 
-# Parametry symulacji
-num_steps = 100  # liczba kroków czasowych
-dt = 0.01  # krok czasowy
-
-# Uruchomienie symulacji
-final_concentration = simulate_pollution_spread(data, num_steps, dt)
-
-# Wizualizacja końcowego stężenia
-plt.imshow(final_concentration, cmap='hot', interpolation='nearest')
-plt.colorbar()
-plt.title("Końcowe stężenie zanieczyszczeń")
-plt.show()
+final_concentration = simulate_pollution_spread(data_2, num_steps, dt, debug=True)
---
+++ calc_model/models/euler_volume_muliboxes_model/grid_and_interpolation.py
@@ -0,0 +1,311 @@
+import numpy as np
+from scipy.spatial import KDTree
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+def create_uniform_boxes(data, box_size=0.01, min_box_size=0.001, max_box_size=0.02, margin=0.005):
+    """
+    Tworzy siatkę pudełek o rozmiarze opartym na średniej odległości między punktami pomiarowymi,
+    uwzględniając temperaturę, prędkość wiatru oraz kierunek wiatru.
+    
+    Parametry:
+    - data: lista punktów pomiarowych (zawiera latitude, longitude, temperature, wind_speed, wind_direction)
+    - min_box_size: minimalny rozmiar pudełka (w stopniach)
+    - max_box_size: maksymalny rozmiar pudełka (w stopniach)
+    - margin: dodatkowy margines na granicach (w stopniach)
+    
+    Zwraca:
+    - boxes: lista granic geograficznych pudełek w formacie [(lat_min, lat_max, lon_min, lon_max), ...]
+    - box_values: wartości pomiarów przypisane do pudełek (temperatura, u, v)
+    """
+
+    latitudes = np.array([point["latitude"] for point in data])
+    longitudes = np.array([point["longitude"] for point in data])
+    temperatures = np.array([point["temperature"] for point in data])
+    wind_speeds = np.array([point["wind_speed"] for point in data])
+    wind_directions = np.array([point["wind_direction"] for point in data])
+    
+
+    """
+    Kierunek wiatru podawany jest w stopniach, co oznacza, że np. kierunek północny to 0 st.
+    Aby rozłożyć kierunek wiatru na składowe u, v (nie uwzględniamy kierunku pionowego - pomiary na stałej wysokości),
+    zamieniamy stopnie na radiany i obliczamy składowe zgodnie z:
+        u=V⋅sin(θ)
+        v=V⋅cos(θ)
+    """
+    wind_directions_rad = np.deg2rad(wind_directions)  # Konwersja stopni na radiany
+    u_values = wind_speeds * np.sin(wind_directions_rad)  # Składowa w kierunku x
+    v_values = wind_speeds * np.cos(wind_directions_rad)  # Składowa w kierunku y
+
+    lat_min, lat_max = np.min(latitudes), np.max(latitudes)
+    lon_min, lon_max = np.min(longitudes), np.max(longitudes)
+    
+    lat_min -= margin
+    lat_max += margin
+    lon_min -= margin
+    lon_max += margin
+    
+    lat_diffs = np.diff(np.sort(latitudes))
+    lon_diffs = np.diff(np.sort(longitudes))
+    
+    avg_lat_diff = np.mean(lat_diffs) if len(lat_diffs) > 0 else max_box_size
+    avg_lon_diff = np.mean(lon_diffs) if len(lon_diffs) > 0 else max_box_size
+    
+    box_size_lat = np.clip(avg_lat_diff, min_box_size, max_box_size)
+    box_size_lon = np.clip(avg_lon_diff, min_box_size, max_box_size)
+    
+    num_lat_boxes = int(np.ceil((lat_max - lat_min) / box_size_lat))
+    num_lon_boxes = int(np.ceil((lon_max - lon_min) / box_size_lon))
+    
+    boxes = []
+    temperature_values = np.full((num_lat_boxes, num_lon_boxes), None)
+    u_grid = np.full((num_lat_boxes, num_lon_boxes), None)
+    v_grid = np.full((num_lat_boxes, num_lon_boxes), None)
+    
+    # obliczanie indeksów pudełek do których przypisujemy dane pomiarowe
+    # lat - lat_min określa położenie na szerokości geograficzniej danego punktu pomiarowego jako odległość o poczatku siatki,
+    # po czym podzielenie przez szerokość pudełka i zaokraglenie w górę daje nam index pudełka do którego przypisujemy zmierzone wartości 
+    for lat, lon, temp, u, v in zip(latitudes, longitudes, temperatures, u_values, v_values):
+        lat_idx = int((lat - lat_min) / box_size_lat)
+        lon_idx = int((lon - lon_min) / box_size_lon)
+        
+        if 0 <= lat_idx < num_lat_boxes and 0 <= lon_idx < num_lon_boxes:
+            if temperature_values[lat_idx, lon_idx] is None:
+                temperature_values[lat_idx, lon_idx] = temp
+                u_grid[lat_idx, lon_idx] = u
+                v_grid[lat_idx, lon_idx] = v
+            else:
+                temperature_values[lat_idx, lon_idx] = (temperature_values[lat_idx, lon_idx] + temp) / 2
+                u_grid[lat_idx, lon_idx] = (u_grid[lat_idx, lon_idx] + u) / 2
+                v_grid[lat_idx, lon_idx] = (v_grid[lat_idx, lon_idx] + v) / 2
+
+
+
+    for i in range(num_lat_boxes):
+      for j in range(num_lon_boxes):
+        lat_min_box = lat_min + i * box_size_lat
+        lat_max_box = lat_min + (i + 1) * box_size_lat
+        lon_min_box = lon_min + j * box_size_lon
+        lon_max_box = lon_min + (j + 1) * box_size_lon
+            
+        boxes.append((lat_min_box, lat_max_box, lon_min_box, lon_max_box))
+    
+    return boxes, temperature_values.flatten(), u_grid.flatten(), v_grid.flatten()
+
+def interpolate_empty_boxes(boxes, box_values, max_distance=0.01):
+    """
+    Interpoluje wartości w pustych pudełkach na podstawie sąsiednich pudełek.
+    
+    Parametry:
+    - boxes: lista granic geograficznych pudełek [(lat_min, lat_max, lon_min, lon_max), ...]
+    - box_values: wartości przypisane do pudełek (None dla pustych pudełek)
+    - max_distance: maksymalna odległość do sąsiednich pudełek dla interpolacji
+    
+    Zwraca:
+    - box_values: zaktualizowane wartości dla pudełek po interpolacji
+    """
+    centers = [(0.5 * (box[0] + box[1]), 0.5 * (box[2] + box[3])) for box in boxes]  
+    tree = KDTree(centers)
+    
+    for i, value in enumerate(box_values):
+        if value is None:
+            # tree.query: centers[i] - środek aktualnie rozważanego pudełka
+            #             k=5, szukamy 5 najbliższych sasiadów
+            #             distance_upper_bound=max_distance, uwzględnia podczas szukania tylko sasiadów o określonej oganiczonej odłegłości 
+            dist, idx = tree.query(centers[i], k=5, distance_upper_bound=max_distance)
+            nearby_values = [box_values[j] for j in idx if j < len(box_values) and box_values[j] is not None]
+            
+            if nearby_values:
+                box_values[i] = np.mean(nearby_values)
+    
+    return box_values
+
+def recursive_interpolation_until_filled(boxes, temp_values, u_values, v_values, initial_distance=0.01, max_increment=0.01):
+    """
+    Rekursywna interpolacja dla temperatury i prędkości wiatru, która kontynuuje, dopóki wszystkie pudełka nie zostaną wypełnione wartościami.
+    
+    Parametry:
+    - boxes: lista granic geograficznych pudełek
+    - temp_values, u_values, v_values: wartości przypisane do pudełek (None dla pustych)
+    - initial_distance: początkowa maksymalna odległość do sąsiednich pudełek
+    - max_increment: krok zwiększający maksymalną odległość sąsiadów w każdej iteracji
+    
+    Zwraca:
+    - zaktualizowane temp_values, u_values, v_values
+    """
+    distance = initial_distance
+    while any(value is None for value in temp_values) or any(value is None for value in u_values) or any(value is None for value in v_values):
+        temp_values = interpolate_empty_boxes(boxes, temp_values, max_distance=distance)
+        u_values = interpolate_empty_boxes(boxes, u_values, max_distance=distance)
+        v_values = interpolate_empty_boxes(boxes, v_values, max_distance=distance)
+        distance += max_increment
+    
+    return temp_values, u_values, v_values
+
+
+def plot_temperature_grid(boxes, temp_values, measurements, save_image=False, image_path=None):
+    """
+    Rysuje siatkę pudełek z interpolowanymi wartościami oraz opcjonalnie zapisuje obraz.
+    
+    Parametry:
+    - boxes: lista granic geograficznych pudełek [(lat_min, lat_max, lon_min, lon_max), ...]
+    - temp_values: wartości temperatury przypisane do pudełek (None dla pustych pudełek)
+    - measurements: lista punktów pomiarowych
+    - save_image: boolean, jeśli True, zapisuje obraz do pliku
+    - image_path: ścieżka do pliku, w którym zapisany zostanie obraz (jeśli save_image=True)
+    """
+    
+    fig, ax = plt.subplots(figsize=(10, 8))
+
+    # normalizacja 
+    valid_values = [value for value in temp_values if value is not None]
+    if valid_values:
+        min_value, max_value = min(valid_values), max(valid_values)
+    else:
+        min_value, max_value = 0, 1
+
+    for i, (lat_min, lat_max, lon_min, lon_max) in enumerate(boxes):
+        color_value = temp_values[i]
+        if color_value is not None:
+            normalized_value = (color_value - min_value) / (max_value - min_value)
+            color = plt.cm.coolwarm(normalized_value)  
+        else:
+            color = 'lightgrey'
+        
+        rect = plt.Rectangle((lon_min, lat_min), lon_max - lon_min, lat_max - lat_min, 
+                             linewidth=1, edgecolor='blue', facecolor=color)
+        ax.add_patch(rect)
+
+        center_lat = 0.5 * (lat_min + lat_max)
+        center_lon = 0.5 * (lon_min + lon_max)
+        if color_value is not None:
+            plt.text(center_lon, center_lat, f'{color_value:.1f}', ha='center', va='center', fontsize=8, color='black', fontweight='bold')
+
+    latitudes = np.array([point["latitude"] for point in measurements])
+    longitudes = np.array([point["longitude"] for point in measurements])
+    temperatures = np.array([point["temperature"] for point in measurements])
+    plt.scatter(longitudes, latitudes, c=temperatures, cmap='coolwarm', edgecolor='k', s=100, zorder=5)
+
+    ax.set_xlabel('Longitude')
+    ax.set_ylabel('Latitude')
+    ax.set_title('Uniform Grid Boxes with Interpolated Values')
+    plt.colorbar(label='Temperature')
+    plt.grid(True)
+
+    if save_image and image_path:
+        plt.savefig(image_path)
+
+def plot_wind_grid(boxes, u_values, v_values, measurements, save_image=False, image_path=None):
+    """
+    Rysuje siatkę pudełek z prędkością i kierunkiem wiatru (u, v) oraz opcjonalnie zapisuje obraz.
+    
+    Parametry:
+    - boxes: lista granic geograficznych pudełek [(lat_min, lat_max, lon_min, lon_max), ...]
+    - u_values, v_values: wartości przypisane do pudełek (składowe prędkości wiatru u, v)
+    - measurements: lista punktów pomiarowych (zawiera latitude, longitude i wind_speed, wind_direction)
+    - save_image: boolean, jeśli True, zapisuje obraz do pliku
+    - image_path: ścieżka do pliku, w którym zapisany zostanie obraz (jeśli save_image=True)
+    """
+    
+    fig, ax = plt.subplots(figsize=(10, 8))
+
+    u_values = np.asarray(u_values, dtype=float)
+    v_values = np.asarray(v_values, dtype=float)
+
+
+    wind_speeds = np.sqrt([np.square(u_values) + np.square(v_values)])  # Obliczamy prędkość wiatru jako |V| = sqrt(u^2 + v^2)
+    
+    valid_wind_speeds = [ws for ws in wind_speeds if ws is not None]
+    valid_wind_speeds = wind_speeds[np.isfinite(wind_speeds)]
+
+    
+    if valid_wind_speeds.size > 0:
+        min_speed, max_speed = min(valid_wind_speeds), max(valid_wind_speeds)
+    else:
+        min_speed, max_speed = 0, 1
+
+    
+    for i, (lat_min, lat_max, lon_min, lon_max) in enumerate(boxes):
+        u_value = u_values[i]
+        v_value = v_values[i]
+        wind_speed = np.sqrt(u_value**2 + v_value**2) if u_value is not None and v_value is not None else None
+        
+        if wind_speed is not None:
+            if min_speed == max_speed:
+                normalized_speed = 0
+            else:
+                normalized_speed = (wind_speed - min_speed) / (max_speed - min_speed)
+            color = plt.cm.viridis(normalized_speed)  # Używamy palety kolorów 'viridis'
+        else:
+            color = 'lightgrey'
+        
+        rect = plt.Rectangle((lon_min, lat_min), lon_max - lon_min, lat_max - lat_min, 
+                             linewidth=1, edgecolor='blue', facecolor=color)
+        ax.add_patch(rect)
+
+        center_lat = 0.5 * (lat_min + lat_max)
+        center_lon = 0.5 * (lon_min + lon_max)
+        
+        lat_min, lat_max, lon_min, lon_max = boxes[0]
+        box_size = np.sqrt((lon_max - lon_min)**2 + (lat_max - lat_min)**2)
+        arrow_scale = box_size * 300
+
+        if u_value is not None and v_value is not None:
+            ax.quiver(center_lon, center_lat, u_value, v_value, scale=arrow_scale, scale_units='inches', color='black', zorder=5)
+
+    latitudes = np.array([point["latitude"] for point in measurements])
+    longitudes = np.array([point["longitude"] for point in measurements])
+    wind_speeds = np.array([point["wind_speed"] for point in measurements])
+    
+    plt.scatter(longitudes, latitudes, c=wind_speeds, cmap='viridis', edgecolor='k', s=100, zorder=5)
+
+    ax.set_xlabel('Longitude')
+    ax.set_ylabel('Latitude')
+    ax.set_title('Wind Speed and Direction Grid')
+    plt.colorbar(label='Wind Speed (m/s)')
+    plt.grid(True)
+
+    if save_image and image_path:
+        plt.savefig(image_path)
+    
+def create_multibox_grid_with_interpolated_measurements(measurements, 
+                                                        box_size=0.01, 
+                                                        min_box_size=0.001, 
+                                                        max_box_size=0.02, 
+                                                        margin=0.005, 
+                                                        save_grid_images=False,
+                                                        save_path=None):
+    """
+    Tworzy siatkę pudełek z interpolowanymi pomiarami oraz opcjonalnie zapisuje obraz z wizualizacją.
+    
+    Parametry:
+    - measurements: lista punktów pomiarowych (zawiera latitude, longitude, temperature, wind speed oraz wind direction)
+    - box_size: początkowy rozmiar pudełka (w stopniach)
+    - min_box_size: minimalny rozmiar pudełka (w stopniach)
+    - max_box_size: maksymalny rozmiar pudełka (w stopniach)
+    - margin: dodatkowy margines na granicach (w stopniach)
+    - save_grid_images: boolean, jeśli True, zapisuje obrazy do plików
+    - image_path: ścieżka do pliku, w którym zapisany zostanie obraz (jeśli save_grid_image=True)
+
+    Zwraca:
+    - boxes: lista granic geograficznych pudełek w formacie [(lat_min, lat_max, lon_min, lon_max), ...]
+    - box_values: wartości pomiarów przypisane do pudełek (po interpolacji wartości pomiędzy pudełkami)
+    """
+    
+    boxes, temp_values, u_values, v_values = create_uniform_boxes(measurements, 
+                                                                  box_size=box_size, 
+                                                                  min_box_size=min_box_size, 
+                                                                  max_box_size=max_box_size, 
+                                                                  margin=margin)
+    
+    temp_values, u_values, v_values = recursive_interpolation_until_filled(boxes, temp_values, u_values, v_values)
+    
+    if save_grid_images:
+        image_path_wind_plot = f'{save_path}/multibox_grid_with_interpolated_wind_values.png'
+        image_path_temp_plot = f'{save_path}/multibox_grid_with_interpolated_temp_values.png'
+        plot_temperature_grid(boxes, temp_values, measurements, save_image=save_grid_images, image_path=image_path_temp_plot)
+        plot_wind_grid(boxes, u_values, v_values, measurements, save_image=save_grid_images, image_path=image_path_wind_plot)
+
+    return boxes, temp_values, u_values, v_values
+
+
